0a2ab1a818c63116785cdb750ce5188a8587e283
Percy Reyes <alfredocancer@gmail.com>
2022-11-15 15:33:03 -0500

    Cambios en la URL de la API

--- client/src/api/tareas.api.js
+++ client/src/api/tareas.api.js
@@ -1,20 +1,20 @@
 import axios from 'axios'
 
 export const listarTareasReques= async()=>{
-  return await axios.get('http://localhost:4000/tareas/listar')
+  return await axios.get('https://crud-tareas-production.up.railway.app//tareas/listar')
 }
 export const crearTareaReques= async(tarea)=>{
-  return await axios.post('http://localhost:4000/tareas/agregar', tarea,);
+  return await axios.post('https://crud-tareas-production.up.railway.app//tareas/agregar', tarea,);
 }
 export const eliminarTareaReques= async(id)=>{
-  return await axios.delete(`http://localhost:4000/tareas/eliminar/${id}`)
+  return await axios.delete(`https://crud-tareas-production.up.railway.app//tareas/eliminar/${id}`)
 }
 export const mostrarTareaReques = async(id)=>{
-  return await axios.get(`http://localhost:4000/tareas/lista/${id}`)
+  return await axios.get(`https://crud-tareas-production.up.railway.app//tareas/lista/${id}`)
 }
 export const actualizarTareReques = async(id, valores)=>{
-  return await axios.put(`http://localhost:4000/tareas/editar/${id}`, valores)
+  return await axios.put(`https://crud-tareas-production.up.railway.app//tareas/editar/${id}`, valores)
 }
 export const cambiarEstadoTareaReques = async(id, estado) =>{
-  return await axios.put(`http://localhost:4000/tareas/editar/${id}`, {estado,})
+  return await axios.put(`https://crud-tareas-production.up.railway.app//tareas/editar/${id}`, {estado,})
 }
