93c0cfaba70d46e4b63be61fa668148665f54fde
Percy Reyes <alfredocancer@gmail.com>
2022-11-15 23:30:44 -0500

    Restableceiendo URL API

--- client/src/api/tareas.api.js
+++ client/src/api/tareas.api.js
@@ -1,20 +1,20 @@
 import axios from 'axios'
 
 export const listarTareasReques= async()=>{
-  return await axios.get('https://crud-tareas-production.up.railway.app:8012/tareas/listar')
+  return await axios.get('https://crud-tareas-production.up.railway.app/tareas/listar')
 }
 export const crearTareaReques= async(tarea)=>{
-  return await axios.post('https://crud-tareas-production.up.railway.app:8012/tareas/agregar', tarea,);
+  return await axios.post('https://crud-tareas-production.up.railway.app/tareas/agregar', tarea,);
 }
 export const eliminarTareaReques= async(id)=>{
-  return await axios.delete(`https://crud-tareas-production.up.railway.app:8012/tareas/eliminar/${id}`)
+  return await axios.delete(`https://crud-tareas-production.up.railway.app/tareas/eliminar/${id}`)
 }
 export const mostrarTareaReques = async(id)=>{
-  return await axios.get(`https://crud-tareas-production.up.railway.app:8012/tareas/lista/${id}`)
+  return await axios.get(`https://crud-tareas-production.up.railway.app/tareas/lista/${id}`)
 }
 export const actualizarTareReques = async(id, valores)=>{
-  return await axios.put(`https://crud-tareas-production.up.railway.app:8012/tareas/editar/${id}`, valores)
+  return await axios.put(`https://crud-tareas-production.up.railway.app/tareas/editar/${id}`, valores)
 }
 export const cambiarEstadoTareaReques = async(id, estado) =>{
-  return await axios.put(`https://crud-tareas-production.up.railway.app:8012/tareas/editar/${id}`, {estado,})
+  return await axios.put(`https://crud-tareas-production.up.railway.app/tareas/editar/${id}`, {estado,})
 }
